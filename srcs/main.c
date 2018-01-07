eaf4e57c04253b04f74f3b0708f5ca73c780db72
Mathieu PINSON <mpinson@e1r8p21.42.fr>
2018-01-07 17:27:45 +0100

    ok

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mpinson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/02 18:08:04 by mpinson           #+#    #+#             */
-/*   Updated: 2017/09/09 18:21:47 by mpinson          ###   ########.fr       */
+/*   Updated: 2018/01/07 17:26:23 by mpinson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ft_menu(t_gen *g)
 	mlx_string_put(g->mlx, g->win, 0, 60, 0x00FFFFFF, "page up contrast");
 	mlx_string_put(g->mlx, g->win, 0, 75, 0x00FFFFFF, "7,4,8,5,9,6 couleur");
 	mlx_string_put(g->mlx, g->win, 0, 90, 0x00FFFFFF, "echap quitte");
+	mlx_string_put(g->mlx, g->win, 0, 105, 0x00FFFFFF, "@ screen");
 }
 
 void	ft_start_algo(t_gen *g, int *i, int *y, int color)
